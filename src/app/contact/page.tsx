73232066c8269e7b9feec7c30fa183413cf77633
edwxrdbobb <edwardbobkamara@gmail.com>
2024-11-30 16:52:17 +0000

    contcat fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,25 @@
+import ContactForm from "@/components/ContactForm";
 
 
 export default function Contact(){
 
     return(
         <>
-            <Contact />
+            
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
+          <div className="max-w-3xl mx-auto text-center">
+            <h2 className="text-3xl font-extrabold text-gray-900">Contact Us</h2>
+            <p className="mt-4 text-lg text-gray-500">
+              Have questions or want to learn more? Get in touch with us!
+            </p>
+          </div>
+          <div className="mt-12 w-full flex justify-center align-center">
+            {/* contact form */}
+            <ContactForm />
+          </div>
+        </div>
+      </div>
         </>
     )
 }
